30eb961540f2a773750457ef8b6a935e267d24bf
Arsen Nurshaikhy <anurshai@sfu.ca>
2023-05-03 16:59:04 -0700

    Added debouncing for searching on client to save api calls and added build to server

--- client/src/Search/SearchBar.jsx
+++ client/src/Search/SearchBar.jsx
@@ -2,16 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
+import debounce from 'lodash/debounce';
 import allowedMovies from './data/allowed_movies.json';
 import ImageDisplay from '../ImageDisplay/ImagesButtons';
 import Image from '../ImageDisplay/Image';
 import './SearchBar.css';
-
-// const imageUrls = [
-//   'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg',
-//   'https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg',
-//   'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg'
-// ];
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -37,21 +32,27 @@ const Search = () => {
   useEffect(() => {
     const fetchMovieTitles = async () => {
       try {
-        const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+        const response = await axios.get('http://localhost:5050/search', {
           params: {
-            api_key: '8baba8ab6b8bbe247645bcae7df63d0d',
             query: searchTerm
           }
         });
-        const matches = response.data.results.filter(movie => allowedMovies.movies.includes(movie.title));
-        const titles = matches.map(movie => movie.title);
-        setOptions(titles);
+        setOptions(response.data);
       } catch (error) {
         console.error(error);
       }
-    };      
-    fetchMovieTitles();
+    };
+
+    // Debounce the API call
+    const debouncedFetchMovieTitles = debounce(fetchMovieTitles, 500);
+    debouncedFetchMovieTitles();
+
+    // Cancel the previous API call when the searchTerm changes
+    return () => {
+      debouncedFetchMovieTitles.cancel();
+    };
   }, [searchTerm]);
+
 
   const handleFormSubmit = event => {
     event.preventDefault();
--- server/server.js
+++ server/server.js
@@ -1,25 +1,48 @@
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
+const apicache = require('apicache');
 const mongoose = require('mongoose');
 const Movie = require('./models/movieModel');
 require('dotenv').config();
 
 const app = express();
+const cache = apicache.middleware;
 
 // Set up CORS middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json());  
 
-// Define API routes
-app.get('/', (req, res) => {
-  const data = { message: 'Default page API' };
-  res.json(data);
-});
+///////// FOR PRODUCTION ONLY ////////
+// Serve static files from the React app
+// app.use(express.static(path.join(__dirname, 'build')));
+//////////////////////////////////////
 
+// Testing
 app.get('/api', (req, res) => {
   const data = { message: 'Hello from the API!' };
   res.json(data);
 });
+
+app.get('/search', cache('5 minutes'), async (req, res) => {
+  const { query } = req.query;
+  try {
+    const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+      params: {
+        api_key: '8baba8ab6b8bbe247645bcae7df63d0d',
+        query: query
+      }
+    });
+    const matches = response.data.results.map(movie => movie.title);
+    res.json(matches);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching data');
+  }
+});
+
+
 
 app.post('/uploadMovie', async (req, res) => {
   try {
